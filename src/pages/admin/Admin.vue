e7bfd6f415c6d1c4e84f483f77e4151fddc69a51
nimdean <fungquanChan@163.com>
2018-09-12 23:46:11 +0800

    首次提交文件

---
+++ src/pages/admin/Admin.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="admin">
+    <h4>后台管理部分</h4>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
